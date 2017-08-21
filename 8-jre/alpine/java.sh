f8fe8657d99e137f786826bf7ae83b016c504aa8
Stefan Fussenegger <stf+git@molindo.at>
2017-08-21 12:30:31 +0200

    use UseCGroupMemoryLimitForHeap by default

--- 8-jre/alpine/java.sh
+++ 8-jre/alpine/java.sh
@@ -3,4 +3,4 @@
 # run java inside docker as user nobody, accepts JAVA_OPTS and JAVA_ARGS env variables
 #
 
-exec /usr/local/bin/gosu "$USER_SPEC" "$JAVA_HOME/bin/java" $JAVA_OPTS "$@" $JAVA_ARGS
+exec /usr/local/bin/gosu "$USER_SPEC" "$JAVA_HOME/bin/java" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap $JAVA_OPTS "$@" $JAVA_ARGS

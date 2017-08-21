c5b4911bc580f8f7552fff0635010885e94bebfb
Stefan Fussenegger <stf+git@molindo.at>
2017-08-21 13:58:39 +0200

    base directly on alpine:3.6

--- 8-jre/alpine/java.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# run java inside docker as user nobody, accepts JAVA_OPTS and JAVA_ARGS env variables
-#
-
-exec /usr/local/bin/gosu "$USER_SPEC" "$JAVA_HOME/bin/java" -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap $JAVA_OPTS "$@" $JAVA_ARGS
